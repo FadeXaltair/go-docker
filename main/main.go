578545eaea0454ebde078c6cbe4869321790ea98
Hitesh Rana <hitesh@xenonstack.com>
2023-06-10 14:59:12 +0530

    first init

---
+++ main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "tetsing hot relrexoadfdsf",
+		})
+	})
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
